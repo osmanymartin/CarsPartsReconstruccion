425cdff4fd0b7f782161b00cd5212c60720bfbdd
OSMANY SAN MARTIN RUBI <osmany.giral@gmail.com>
2016-06-18 13:29:48 -0400

    adding the users roles
    adding the users roles

---
+++ CarsPartsReconstruccion/Controllers/UsersController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarsPartsReconstruccion.Models;
+
+namespace CarsPartsReconstruccion.Controllers
+{
+    public class UsersController : Controller
+    {
+        private db_cars_parts_reconstructionStrConn db = new db_cars_parts_reconstructionStrConn();
+
+        //
+        // GET: /Users/
+
+        public ActionResult Index()
+        {
+            return View(db.UserProfiles.Include(us => us.webpages_Roles).ToList());
+        }
+
+        //
+        // GET: /Users/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userprofile);
+        }
+
+        //
+        // GET: /Users/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userprofile);
+        }
+
+        //
+        // POST: /Users/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UserProfile userprofile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userprofile).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userprofile);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
