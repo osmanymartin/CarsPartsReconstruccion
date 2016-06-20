b74d24a76bfa7c773f1a1369ac36f01066280260
OSMANY SAN MARTIN RUBI <osmany.giral@gmail.com>
2016-06-20 17:49:30 -0400

    do the ajax asynchronous with jquery
    do the ajax asynchronous with jquery

--- CarsPartsReconstruccion/App_Start/BundleConfig.cs
+++ CarsPartsReconstruccion/App_Start/BundleConfig.cs
@@ -12,7 +12,8 @@ namespace CarsPartsReconstruccion
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/jquery-ui-{version}.js",
                         "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/carpart.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
---
+++ CarsPartsReconstruccion/Scripts/carpart.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    var ajaxFormSubmit = function () {
+        debugger;
+        var $form = $(this);
+
+        var options = {
+            url: $form.attr("action"),
+            type: $form.attr("method"),
+            data: $form.serialize()
+        };
+
+        $.ajax(options).done(function (data) {
+            var $target = $($form.attr("data-carpart-target"));
+            $target.replaceWith(data);
+        });
+
+        return false;
+    }
+
+    $("form[data-carpart-ajax='true']").submit(ajaxFormSubmit);
+});
