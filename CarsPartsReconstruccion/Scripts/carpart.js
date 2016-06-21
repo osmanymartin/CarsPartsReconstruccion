2e103f4ee406367d04ad26134300ce4ff54cdc39
OSMANY SAN MARTIN RUBI <osmany.giral@gmail.com>
2016-06-21 16:52:52 -0400

    paged list finish

--- CarsPartsReconstruccion/Scripts/carpart.js
+++ CarsPartsReconstruccion/Scripts/carpart.js
@@ -41,7 +41,26 @@
         $input.autocomplete(options);
     };
 
+    var getPage = function () {
+        var $a = $(this);
+
+        var options = {
+            url: $a.attr("href"),
+            data: $("form").serialize(),
+            type: "get"
+        }
+
+        $.ajax(options).done(function (data) {
+            var target = $a.parents("div.pagedList").attr("data-carpart-target");
+            $(target).replaceWith(data);
+        });
+
+        return false;
+    };
+
     $("form[data-carpart-ajax='true']").submit(ajaxFormSubmit);
 
     $("input[data-carpart-autocomplete]").each(createAutocomplete);
+
+    $(".main-content").on("click", ".pagedList a", getPage);
 });
